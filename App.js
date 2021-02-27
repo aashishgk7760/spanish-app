c7b2820453b756ac628c2c61175d3d9aebd62c60
aashishgk7760 <aashishgk601@gmail.com>
2021-02-27 00:57:36 -0600

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Audio } from "expo-av";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import Sound from "react-native-sound";
+const soundList = [
+  require("./assets/one.wav"),
+  require("./assets/two.wav"),
+  require("./assets/three.wav"),
+  require("./assets/four.wav"),
+  require("./assets/five.wav"),
+  require("./assets/six.wav"),
+  require("./assets/seven.wav"),
+  require("./assets/eight.wav"),
+  require("./assets/nine.wav"),
+  require("./assets/ten.wav"),
+];
 
 export default function App() {
+  const playSound = (sound) => {
+    const soundVar = new Sound(sound, Sound.MAIN_BUNDLE, (err) => {
+      if (err) {
+        console.log("No Sound");
+      }
+    });
+    setTimeout(() => {
+      soundVar.play();
+    }, 1000);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <Image style={styles.logo} source={require("./assets/logo.png")}></Image>
+      <View style={styles.gridContainer}>
+        {soundList.map((sound) => (
+          <TouchableOpacity
+            key={sound}
+            style={styles.box}
+            onPress={() => {
+              playSound();
+            }}
+          >
+            <Text style={styles.text}>{sound}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1b262c",
+  },
+  logo: {
+    alignSelf: "center",
+    marginTop: 15,
+  },
+  gridContainer: {
+    flex: 1,
+    margin: 5,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "flex-start",
+    justifyContent: "space-around",
+  },
+  box: {
+    height: 110,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "46%",
+    marginVertical: 6,
+    backgroundColor: "#0f4c75",
+    borderRadius: 5,
+
+    shadowColor: "#393e46",
+    elevation: 5,
+  },
+  text: {
+    fontSize: 50,
+    color: "#ff4301",
   },
 });
